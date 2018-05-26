05051cd5d9db9118ce06c7e7d608dd8c75443a01
EC2 Default User <ec2-user@ip-172-31-26-66.us-east-2.compute.internal>
2018-05-26 15:01:07 +0000

    angular build

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express(),
 path = require('path'),
-publicDir = path.join(__dirname,'public');
+publicDir = path.join(__dirname,'web','dist');
 
 app.use(express.static(publicDir))
 
