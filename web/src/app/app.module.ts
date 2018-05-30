09d5dd1202ef9851daee3fcf3a0fd1872d3490bc
hemi <hemico@post.jce.ac.il>
2018-05-30 21:19:47 +0300

    slim

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -11,6 +11,11 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { UsersComponent } from './users/users.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { OfferComponent } from './offer/offer.component';
+import { ActivityComponent } from './activity/activity.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 
 @NgModule({
@@ -19,7 +24,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     NavigationComponent,
     UsersComponent,
     NotFoundComponent,
-    DashboardComponent
+    DashboardComponent,
+    SignUpComponent,
+    SignInComponent,
+    OfferComponent,
+    ActivityComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
@@ -27,8 +37,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     AngularFireDatabaseModule,
     HttpModule,
     RouterModule.forRoot([
-      {path: '', component: UsersComponent},
+      {path: '', component: HomePageComponent},
       {path: 'users', component: UsersComponent},
+      {path: 'activity', component: ActivityComponent},
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'offer', component: OfferComponent},
+      {path: 'sign-in', component: SignInComponent},
+      {path: 'sign-up', component: SignUpComponent},
       {path: '**', component: NotFoundComponent}
     ])
 
